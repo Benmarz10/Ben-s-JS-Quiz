e273bf08677c0727c40881ffb70a8150513fe1d1
Benmarz10 <benjaminmarz10@gmail.com>
2022-11-16 17:22:21 -0600

    finished my styling so that all we see as of right now is the start button, started psuedo coding for the diff functions necessary for assignment

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+//write the startgame func
+function start() {
+
+}
+//next question func
+function nextQuestion() {
+
+}
+//answer selection func
+function answerSelect() {
+
+}
+
+//timer func so when we click start it starts game and also subtracts time if answerd wrong
+
+//when game is over by answering all questions or timer hits zero then we can sign initials and store a highscore
